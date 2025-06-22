cdb9b98e8052bd9851e79872551cea6c5a1afe5a
Patrikol4 <doublecrt@gmail.com>
2025-06-22 20:17:40 -0300

    config principal

--- src/config.js
+++ src/config.js
@@ -7,12 +7,12 @@ exports.PREFIX = "/";
 exports.BOT_EMOJI = "🤖";
 
 // Nome do bot (mude se preferir).
-exports.BOT_NAME = "Takeshi Bot";
+exports.BOT_NAME = "Servo de Cristo Bot";
 
 // Número do bot. Coloque o número do bot
 // (apenas números, exatamente como está no WhatsApp).
 // Se o seu DDD não for de SP ou do Rio, não coloque o 9 antes do número.
-exports.BOT_NUMBER = "558112345678";
+exports.BOT_NUMBER = "5519989510911";
 
 // Número do dono do bot. Coloque o número do dono do bot
 // (apenas números, exatamente como está no WhatsApp).
